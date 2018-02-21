9c4779625b1f704a102dc4bd2d3320c9405ca36e
Ranjith Kumar S <ranjithkumars94@gmail.com>
2018-02-21 17:27:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('EXOS docker jenkins pipeline testing..2222')
+  response.send('EXOS docker/jenkins pipeline testing..3333')
 })
 
 app.listen(app.get('port'), function() {
